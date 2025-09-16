7458561df78f17376d258e83eedfc2875ae4cc77
anastasiiiii <nastia262002@gmail.com>
2025-09-16 21:23:20 +0200

    addcnfjdc

--- backend/src/main/java/com/apyrio/backend/BackendApplication.java
+++ backend/src/main/java/com/apyrio/backend/BackendApplication.java
@@ -9,6 +9,7 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		print('Hello world')
+		print('Hello world')
 	}
 
 }
